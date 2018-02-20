1d18181a27ae4cfff2234960ae8c1ed26185f203
speckospock <17017460+speckospock@users.noreply.github.com>
2018-02-20 11:27:40 -0800

    Implements user interaction

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const makeTile = el => {
   if (el < 0) return 'O';
   if (el > 0) return 'X';
@@ -14,14 +13,41 @@ const createBoard = boardArr => {
     board += createRow(row);
     board += fillRow;
   });
+  console.log(board);
+  console.log('Make a move in the format \'row,col\':');
   return board;
 };
 
 const initialBoard = () => ([[0, 0, 0],[0, 0, 0],[0, 0, 0]]);
 
+const createMoveHandler = () => {
+  let player = 1;
+  return (move, board) => {
+    let r = move[0];
+    let c = move[2];
+    if (r >= 0 && r <= 2 && c >= 0 && c <= 2) {
+      board[r][c] = player;
+      player = (player > 0) ? -1 : 1;
+    }
+    return board;
+  }
+}
+
+const handleMove = createMoveHandler();
+
+const play = (board = initialBoard()) => {
+  createBoard(board);
+  return board;
+}
+
 const ticTacToe = () => {
-  let board = createBoard(initialBoard());
-  console.log(board);
+  let gameBoard = initialBoard();
+  process.stdin.on('data', (data) => {
+    let move = data.toString().trim();
+    gameBoard = handleMove(move, gameBoard);
+    createBoard(gameBoard);
+  });
+  let board = play(gameBoard);
 };
 
 ticTacToe();
